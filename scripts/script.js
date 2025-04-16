50a7d8aafd1293449d60fe34c0304ef8549337e5
Lara Pla <laraplamoreno@gmail.com>
2025-04-16 15:25:52 +0200

    feat: modified script

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const button1 = document.getElementById("b1");
+/*const button1 = document.getElementById("b1");
 const button2 = document.getElementById("b2");
 const button3 = document.getElementById("b3");
 const button4 = document.getElementById("b4");
@@ -13,6 +13,24 @@ const button12 = document.getElementById("b12");
 const button13 = document.getElementById("b13");
 const button14 = document.getElementById("b14");
 const button15 = document.getElementById("b15");
+*/
+
+const buttons = []
+
+for (let i= 1; i <= 15; i++) {
+  const button = document.getElementById(`b${i}`);
+  buttons.push(button);
+}
+console.log(buttons)
+
+const sounds = ["577693__joesh2__c2.wav", "577690__joesh2__d2.wav", "577694__joesh2__e2.wav", "577698__joesh2__f2.wav", "577702__joesh2__g2.wav", "577689__joesh2__a2.wav", "577687__joesh2__b2.wav", "577692__joesh2__c3.wav", "577695__joesh2__d3.wav", "577699__joesh2__e3.wav", "577700__joesh2__f3.wav", "577701__joesh2__g3.wav", "577688__joesh2__a3.wav", "577686__joesh2__b3.wav", "577691__joesh2__c4.wav"]
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", () => {
+    sounds[i].play();
+  })
+  console.log(buttons[i])
+}
 
 const sonidoMarimba1 = new Howl({
   src: ["../assets/sounds/577693__joesh2__c2.wav"], // Ruta del sonido
